8e45c6123eccb6211f79e057650a85b7959da4e4
holabisii <ajoseholabisi@gmail.com>
2022-07-04 17:27:42 -0400

    music genre section complete

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -48,6 +48,72 @@ export const Main:React.FC = () => {
                                 </div>
 
                         </div>
+
+                        <div className="main-main2 text-white mt-20 text-center" >
+                                <div>
+                                        <h2 className="font-bold text-4xl tracking-wide">Musical Genres</h2>
+
+                                </div>
+                                <div className="mt-20">
+                                        <div className="ul-main">
+                                                <ul>
+                                                        <li><a href="#jzz">Jazz</a></li>
+                                                        <li><a href="#jzz" className="active" >Hip-Hop</a></li>
+                                                        <li><a href="#jzz">Blues</a></li>
+                                                        <li><a href="#jzz">Rock</a></li>
+                                                        <li><a href="#jzz">Funk</a></li>
+                                                        <li><a href="#jzz">Funk</a></li>
+                                                        <li><a href="#jzz">Funk</a></li>
+                                                </ul>
+                                        </div>
+                                        <div className="ul-main">
+
+                                                <div className="main-content">
+                                                        <div className="main-content-box p-4 text-left ml-8 mr-8">
+                                                                
+                                                                <img src="../../Gen1.png" alt="fir" />
+                                                                <h2 className="font-bold text-2xl p-2 pl-0 tracking-widest ">Are you a Music Artist ?</h2>
+                                                                <p className="tracking-wide text-lg font-bold mb-7">You Can Submit Your Song <br />For Distribution</p>
+                                                                <button className="text-center tracking-wide text-xl font-medium">
+                                                                        <span className="text-4xl pr-4">+</span>
+                                                                        Submit Music
+                                                                </button>
+                                                        </div>
+
+                                                        <div className="main-content-box p-4 text-left ml-8 mr-8">
+                                                                
+                                                                <img src="../../Gen2.png" alt="fir" />
+                                                                <h2 className="font-bold text-2xl p-2 pl-0 tracking-widest ">The Box</h2>
+                                                                <p className="tracking-wide text-lg font-bold mb-7">Roddy Ricch <br />85% Available Stocks </p>
+                                                                <button className="text-center tracking-wide text-xl font-medium">
+                                                                        Execute Order
+                                                                </button>
+                                                        </div>
+                                                        <div className="main-content-box p-4 text-left ml-8 mr-8">
+                                                                
+                                                                <img src="../../Gen1.png" alt="fir" />
+                                                                <h2 className="font-bold text-2xl p-2 pl-0 tracking-widest ">No Guidance</h2>
+                                                                <p className="tracking-wide text-lg font-bold mb-7">Chris Brown ft Drake <br />65% Available Stocks</p>
+                                                                <button className="text-center tracking-wide text-xl font-medium">
+                                                                        Execute Order
+                                                                </button>
+                                                        </div>
+                                                        <div className="main-content-box p-4 text-left ml-8 mr-8">
+                                                                
+                                                                <img src="../../Gen2.png" alt="fir" />
+                                                                <h2 className="font-bold text-2xl p-2 pl-0 tracking-widest ">Are you a Music Artist ?</h2>
+                                                                <p className="tracking-wide text-lg font-bold mb-7">You Can Submit Your Song <br />For Distribution</p>
+                                                                <button className="text-center tracking-wide text-xl font-medium">
+                                                                        <span className="text-4xl pr-4">+</span>
+                                                                        Submit Music
+                                                                </button>
+                                                        </div>
+                                                </div>
+
+
+                                        </div>
+                                </div>
+                        </div>
                 
                 </div>
         )
