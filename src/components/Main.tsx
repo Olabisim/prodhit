e1cbafe0b267f5fe5a6aeb6e13c7ac37809b985d
holabisii <ajoseholabisi@gmail.com>
2022-07-28 14:34:56 -0400

    formatted svgs plus little changes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,5 @@
+import {FirstMainSvg, SecondMainSvg, LandingPageSvg} from '../components/svg/BodySvgs'
+
 
 export const Main:React.FC = () => {
         return (
@@ -22,18 +24,7 @@ export const Main:React.FC = () => {
                                         <div className="">
                                                 <div className="header_svg_container">
 
-                                                <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" width="813.61811" height="712.2585" viewBox="0 0 813.61811 712.2585" xmlnsXlink="http://www.w3.org/1999/xlink">        
-                                                        <polygon points="501.384 26 461.414 91.898 501.298 115.501 501.384 26" fill="#828cfc"/>
-                                                        <polygon points="461.549 92.033 501.298 73.916 501.249 115.529 461.549 92.033" fill="#505efb"/>
-                                                        <polygon points="501.211 26 541.182 91.898 501.298 115.501 501.211 26" fill="#2D3FFB"/>
-                                                        <polygon points="541.047 92.033 501.298 73.916 501.346 115.529 541.047 92.033" fill="#0412af"/>
-                                                        <polygon points="461.549 99.595 501.249 155.5 501.249 123.091 461.549 99.595" fill="#828cfc"/>
-                                                        <polygon points="540.95 99.595 501.249 155.5 501.249 123.091 540.95 99.595" fill="#2D3FFB"/>
-                                                        <path d="M694.80906,296.87075c-21.47217,0-42.86426-14.75928-60.23609-41.55908-15.2749-23.56494-24.76391-52.85547-24.76391-76.44092a85,85,0,1,1,170,0c0,23.58545-9.489,52.876-24.76392,76.44092C737.67331,282.11147,716.28122,296.87075,694.80906,296.87075Zm0-201a83.09384,83.09384,0,0,0-83,83c0,45.80762,37.18383,116,83,116s83-70.19238,83-116A83.09385,83.09385,0,0,0,694.80906,95.87075Z" transform="translate(-193.19094 -93.87075)" fill="#B106F9"/>
-                                                        <path d="M702.82248,314.87075H686.79563a4.99984,4.99984,0,0,1-4.88086-6.08447l3.092-13.91553H704.6113l3.092,13.91553a4.99984,4.99984,0,0,1-4.88086,6.08447Zm-16.21118-18-2.74438,12.34912a3.0002,3.0002,0,0,0,2.92871,3.65088h16.02685a3.0002,3.0002,0,0,0,2.92871-3.65088l-2.74438-12.34912Z" transform="translate(-193.19094 -93.87075)" fill="#B106F9"/>
-                                                        <path d="M655.24913,118.73207a58.4403,58.4403,0,0,0-26.39551,35.90807,56.1951,56.1951,0,0,0-1.62463,12.73165,1.5009,1.5009,0,0,0,3,0,55.38506,55.38506,0,0,1,16.821-38.60748,52.63215,52.63215,0,0,1,9.71327-7.44182,1.50112,1.50112,0,0,0-1.51416-2.59042Z" transform="translate(-193.19094 -93.87075)" fill="#B106F9"/>
-                                                        <path d="M680.91654,298.75515c18.12262-.92765,38.74522-1.422,52.869,11.94557,5.89108,5.57568,9.60778,13.04755,9.38366,21.26143-.24389,8.9385-4.78918,17.31157-10.42206,24.02662-12.12594,14.45556-29.90953,23.49395-46.96391,30.77883-18.126,7.74265-37.00889,13.789-54.42431,23.13207C616.068,418.103,601.81494,429.176,592.57768,444.093a62.48306,62.48306,0,0,0-8.8561,23.75627,67.38471,67.38471,0,0,0,1.26309,25.37213c4.06791,17.01383,14.08316,32.05049,27.42863,43.22353,13.50543,11.307,29.86609,18.83645,46.81635,23.335,20.12215,5.3403,41.08822,6.549,61.821,6.66982,21.41283.12473,42.84346-.923,64.24146.27721,5.20489.29194,10.40073.72292,15.57931,1.323,1.91429.22181,1.8984-2.78,0-3-21.37675-2.477-42.9212-1.90954-64.39379-1.66672-21.0512.23806-42.28077.16475-63.0709-3.552-17.92524-3.20462-35.50032-9.21059-50.70307-19.397-14.56244-9.75735-26.43061-23.43911-32.43634-40.041-5.8664-16.21681-5.805-34.16408,2.05032-49.71034,7.8653-15.5661,21.585-27.34159,36.49353-35.93455,17.20467-9.91642,36.14888-16.12221,54.42574-23.6834C700.5,383.92305,718.157,375.407,731.41794,361.90558c6.14112-6.25251,11.40149-13.86221,13.65264-22.42406a30.33265,30.33265,0,0,0-3.403-23.90111,40.57685,40.57685,0,0,0-17.84641-15.04457c-8.80276-3.96638-18.56975-5.1098-28.14049-5.20641-4.92495-.04971-9.847.174-14.76417.42572-1.92106.09833-1.93276,3.09893,0,3Z" transform="translate(-193.19094 -93.87075)" fill="#B106F9"/>
-                                                </svg>
+                                                <LandingPageSvg />
 
                                                         {/* <div className="w-1/2 m-3 mr-0 nav-landingside-1">
                                                                 <img src="../../Rectangle-1.png" alt="first pic" width='300px' />
@@ -84,7 +75,7 @@ export const Main:React.FC = () => {
                                                 <div className="main-content">
                                                         <div className="main-content-box p-4 text-left ml-8 mr-8">
                                                                 
-                                                                <img src="../../Gen1.png" alt="fir" />
+                                                                <img src="https://images.unsplash.com/photo-1550565118-3a14e8d0386f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1vbmV5fGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="fir" />
                                                                 <h2 className="text-lg lg:text-2xl p-2 pl-0 lg:tracking-wide ">Are you a Music Artist ?</h2>
                                                                 <p className="tracking-wide text-sm lg:text-lg mb-7">You Can Submit Your Song <br />For Distribution</p>
                                                                 <button className="text-center tracking-wide text-sm lg:text-xl font-medium">
@@ -95,7 +86,7 @@ export const Main:React.FC = () => {
 
                                                         <div className="main-content-box p-4 text-left ml-8 mr-8">
                                                                 
-                                                                <img src="../../Gen2.png" alt="fir" />
+                                                                <img src="https://images.unsplash.com/photo-1561414927-6d86591d0c4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9uZXl8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="fir" />
                                                                 <h2 className="text-lg lg:text-2xl p-2 pl-0 lg:tracking-widest">The Box</h2>
                                                                 <p className="tracking-wide text-sm lg:text-lg mb-7">Roddy Ricch <br />85% Available Stocks </p>
                                                                 <button className="text-center tracking-wide text-sm lg:text-xl font-medium">
@@ -104,7 +95,7 @@ export const Main:React.FC = () => {
                                                         </div>
                                                         <div className="main-content-box p-4 text-left ml-8 mr-8">
                                                                 
-                                                                <img src="../../chris3.png" alt="fir" />
+                                                                <img src="https://images.unsplash.com/photo-1550565118-3a14e8d0386f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1vbmV5fGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="fir" />
                                                                 <h2 className="text-lg lg:text-2xl p-2 pl-0 lg:tracking-widest">No Guidance</h2>
                                                                 <p className="tracking-wide text-sm lg:text-lg mb-7">Chris Brown ft Drake <br />65% Available Stocks</p>
                                                                 <button className="text-center tracking-wide text-sm lg:text-xl font-medium">
@@ -113,7 +104,7 @@ export const Main:React.FC = () => {
                                                         </div>
                                                         <div className="main-content-box p-4 text-left ml-8 mr-8">
                                                                 
-                                                                <img src="../../Gen2.png" alt="fir" />
+                                                                <img src="https://images.unsplash.com/photo-1561414927-6d86591d0c4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9uZXl8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="fir" />
                                                                 <h2 className="text-lg lg:text-2xl p-2 pl-0 lg:tracking-widest">Are you a Music Artist ?</h2>
                                                                 <p className="tracking-wide text-sm lg:text-lg font-bold mb-7">You Can Submit Your Song <br />For Distribution</p>
                                                                 <button className="text-center tracking-wide text-sm lg:text-xl font-medium">
@@ -128,30 +119,31 @@ export const Main:React.FC = () => {
                                 </div>
                         </div>
 
-                        <div className="main-main3 text-white p-1">
+                        <div className="main-main3 text-white p-1 pb-16 md:pb-1">
 
                                 <div className="container mx-auto">
                                                 
-                                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-center mt-36 m-12">
+                                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-center m-5 mt-0">
                                                 <div className="flex justify-center">
-                                                        <img src="../../last2.png" alt="last pic" />
+                                                        {/* <img src="../../crypto_portfolio_2jy5.svg" alt="last pic" /> */}
+                                                        <FirstMainSvg />
                                                 </div>
 
-                                                <div className="justify-center w-full pr-10 pt-7 items-center">
+                                                <div className="justify-center w-full md:pr-24 md:pt-7 items-center text-center md:text-left adjust_margintop_about_section">
                                                         <h2 className="text-2xl lg:text-4xl font-normal ">What is <span>Prodhit? </span></h2>
                                                         <br />
-                                                        <p className="text-sm lg:text-xl tracking-wider leading-relaxed font-normal">     Prodhit Is The Fastest Growing Music
+                                                        <p className="text-sm lg:text-xl tracking-wider leading-relaxed font-normal p-10 md:p-0">     Prodhit Is The Fastest Growing Music
                                                                 Platform Where You Can Buy Shares
                                                                 From Songs On Spotify And Also Submit 
                                                                 Your Song For Distribution And Earn.
                                                         </p>
                                                 </div>
                                         </div>
-                                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-center mt-36 m-12 second_grid_adjustment">
-                                                <div className="justify-center w-full pr-10 pt-7 items-center">
+                                        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-center m-5 mt-0 lg:pl-14 second_grid_adjustment -mt-28 md:-mt-20">
+                                                <div className="justify-center w-full md:pr-10 md:pt-7 items-center text-center md:text-left adjust_margintop_about_section2">
                                                         <h2 className="text-2xl lg:text-4xl font-normal ">How it <span>Works </span></h2>
                                                         <br />
-                                                        <p className="text-sm lg:text-xl tracking-wider leading-relaxed font-normal">     
+                                                        <p className="text-sm lg:text-xl tracking-wider leading-relaxed font-normal p-10 md:p-0">     
                                                                 It’s Easy.After Connecting Your Wallet
                                                                 And Buying Shares From Any Song By
                                                                 Clicking On “EXECUTE ORDER”,All Money
@@ -164,7 +156,7 @@ export const Main:React.FC = () => {
                                                         </p>
                                                 </div>
                                                 <div className="flex justify-center">
-                                                        <img src="../../last1.png" alt="last pic" />
+                                                        <SecondMainSvg />
                                                 </div>
 
                                         </div>
@@ -173,7 +165,7 @@ export const Main:React.FC = () => {
                                 
                         </div>
 
-                        <div className="main-main4 text-center text-white mt-28">
+                        <div className="main-main4 text-center text-white mt-24 md:mt-36">
                                 <div>
                                         <h2 className="text-3xl lg:text-4xl tracking-wide mb-5">Our <span>Team</span></h2>
                                         <p className="tracking-wide text-base lg:text-xl" >Our Highly Qualified And Skilled Team</p>
